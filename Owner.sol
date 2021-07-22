c363f17182fb027290d4b45defaffb84de1e3551
BenBktech <bbrucher@gmail.com>
2021-07-22 18:08:01 +0200

    Added Files

---
+++ Owner.sol
@@ -0,0 +1,16 @@
+pragma solidity 0.8.6;
+
+contract Owner {
+    
+    address owner;
+    
+    constructor() {
+        owner = msg.sender;
+    }
+    
+    modifier isOwner() {
+        require(msg.sender == owner, "Not the Owner");
+        _;
+    }
+    
+}
